50d8da31c2c29b840bbb92e1c2a068fb7659d78f
krishna.sashank <krishna.sashank@eebb64ee-bc34-0410-bea6-7bdd59968c7c>
2007-09-12 04:15:08 +0000

    testing scripts modified

--- repos/trunk/test/compile.sh
+++ repos/trunk/test/compile.sh
@@ -1,3 +1,4 @@
-#gcc -o translate `curl-config --cflags` `curl-config --libs` -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -lglib-2.0 translate.c 
-gcc -o read-config-xml `xml2-config --cflags` read-config-xml.c `xml2-config --libs`  -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -lglib-2.0
+# Can some one please write a Makefile for this 
+gcc -o read-config-xml `xml2-config --cflags` ../src/core/xml-translate.c ../src/core/xml-translate.h read-config-xml.c `xml2-config --libs`  -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -lglib-2.0
+#gcc -o translate-test  `curl-config --cflags` `curl-config --libs` `xml2-config --cflags` ../src/core/xml-translate.c ../src/core/xml-translate.h ../src/core/translate.c ../src/core/translate.h translate-test.c `xml2-config --libs`  -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include -lglib-2.0
 
